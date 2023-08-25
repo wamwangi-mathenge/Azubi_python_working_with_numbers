fe47a15b9da3ba26c039f6f3d06c714450a6a75a
Brian <wamwangimathenge@gmail.com>
2023-08-25 11:23:55 +0300

    Perform basic type conversions

--- nums.py
+++ nums.py
@@ -5,3 +5,13 @@ second_num = 2
 print(first_num + second_num)
 print(first_num * second_num)
 print(first_num / second_num)
+
+# Type conversion
+days_in_feb = 28
+print(str(days_in_feb) + " days in February")
+
+# Input function will always return a string
+first_num = input("Enter first number ")
+second_num = input("Enter second number ")
+print(int(first_num) + int(second_num))
+print(float(first_num) + float(second_num))
